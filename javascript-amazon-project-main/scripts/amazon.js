5d68915fb604a1c26270803c68e800d3941054ea
MishaBar <msbarvenko@gmail.com>
2023-10-28 19:56:04 +0300

    make the cart quantity interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -74,10 +74,18 @@ document.querySelectorAll('.js-add-to-cart')
         matchingItem.quantity += 1;
       } else {
         cart.push({
-          productID: productId,
+          productId: productId,
           quantity: 1
         })
       }
 
+      let cartQuantity = 0;
+
+      cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+      })
+
+      document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
     });
    });
