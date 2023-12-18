a69ff3be93e683e568d0d32dc58c17bb81783c53
Rando16 <59538383+Rando16@users.noreply.github.com>
2023-12-18 21:40:51 +0200

    Delete courses directory

--- courses/darina/script.js
+++
@@ -1,8 +0,0 @@
-function ChequeValue() {
-  const darinaButton = document.querySelector('.js-button')
-
-  if (darinaButton.innerHTML === 'Cheque'){
-    darinaButton.innerHTML = 'Tips'
-  } else {darinaButton.innerHTML = 'Cheque'}
-}
-
